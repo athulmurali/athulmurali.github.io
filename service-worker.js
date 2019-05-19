e871b0c39283b942702c34993bfbda81ede9a3d2
Athul Muralidharan <athulmurali@gmail.com>
2019-05-19 13:43:33 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5541c4b0906b4bae7948dba31afc344.js"
+  "/precache-manifest.801b2846ccce0aa974f2dc6249367dae.js"
 );
 
 workbox.clientsClaim();
